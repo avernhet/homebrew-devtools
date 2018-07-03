973e45cb64f52a8514bde325cd4d2a8f907571c9
avernhet <40272175+avernhet@users.noreply.github.com>
2018-07-03 14:20:56 +0200

    Previous wrong syntax

--- sbx-script.rb
+++ sbx-script.rb
@@ -12,7 +12,7 @@ class SbxScript <Formula
   def stage(target=nil, &block)
     bin.mkpath
     path = bin+"sbx"
-    path.write <<~EOS.undent
+    path.write <<~EOS
       #!/bin/sh
       
       SANDBOXES="sandboxes"
